2260107246f0323cc33bba218e9510325dff5f67
Fuvo Roy <fuvoroy@gmail.com>
2026-02-12 15:57:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1908397287-8d51a",
+  "appId": "1:742104897739:web:e081dde2ee50925088634e",
+  "apiKey": "AIzaSyAhsvquZruYInrHJJRXcErAZ9nbJPhtrhg",
+  "authDomain": "studio-1908397287-8d51a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "742104897739"
+};
